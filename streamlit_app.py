1ef762c118d72a40bcda85115e0be71cbd5efd4b
DwaneGarcia <garcia.dwanelloyd@gmail.com>
2025-05-05 13:26:25 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit as st
+import pandas as pd
 
 st.title('Hypertension, Stroke, and Diabetes Predictor')
 
 st.info('HSD is a machine learning web application for simultaneously predicting Hypertension, Stroke, and Diabetes.')
+
+df = pd.read_csv('https://raw.githubusercontent.com/DwaneGarcia/hrd-machinelearning/refs/heads/master/diabetes_data.csv')
+df
