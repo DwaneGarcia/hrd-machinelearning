8bd51627b1f32411ffadf6194ca7e9fedd0589b5
DwaneGarcia <garcia.dwanelloyd@gmail.com>
2025-05-05 13:58:43 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,19 +5,15 @@ st.title('Hypertension, Stroke, and Diabetes Predictor')
 
 st.info('HSD is a machine learning web application for simultaneously predicting Hypertension, Stroke, and Diabetes.')
 
-with st.expander('Data'):
+with st.expander('**Data**'):
   st.write('Raw Data')
   df = pd.read_csv('https://raw.githubusercontent.com/DwaneGarcia/hrd-machinelearning/refs/heads/master/diabetes_data.csv')
   df
 
   st.write('**X**')
-  X = df.drop('Stroke_HighBP_Diabetes', axis=1)
+  X = df.drop('Stroke', axis=1)
   X
 
-  st.write('**y**'_'**z**'_'**a**')
+  st.write('**y**')
   y = df.'Stroke'
   y
-  z = df.'HighBP"
-  z
-  a = df.'Diabetes'
-  a
