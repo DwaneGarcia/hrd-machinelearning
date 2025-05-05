b85058cc7ae1424177a2cdb0fc2e2ab863081a5e
DwaneGarcia <garcia.dwanelloyd@gmail.com>
2025-05-05 13:45:12 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,3 +9,11 @@ with st.expander('Data'):
   st.write('Raw Data')
   df = pd.read_csv('https://raw.githubusercontent.com/DwaneGarcia/hrd-machinelearning/refs/heads/master/diabetes_data.csv')
   df
+
+  st.write('**X**')
+  X = df.drop('Stroke', axis=16)
+  X
+
+  st.write('**y**')
+  y = df.Stroke
+  y
