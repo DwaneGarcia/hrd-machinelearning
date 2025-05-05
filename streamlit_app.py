237bf48f28db57ef188920b9507f156b9e1a9373
DwaneGarcia <garcia.dwanelloyd@gmail.com>
2025-05-05 13:02:37 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 st.title('Hypertension, Stroke, and Diabetes Predictor')
 
-st.info('HSD is a machine learning web application for simultaneously predicting Hypertension, Stroke, and Diabetes')
+st.info('HSD is a machine learning web application for simultaneously predicting Hypertension, Stroke, and Diabetes.')
