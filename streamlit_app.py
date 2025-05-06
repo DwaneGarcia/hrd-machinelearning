45cb50af5bc00b108d498a9c89418551e06047f2
DwaneGarcia <garcia.dwanelloyd@gmail.com>
2025-05-07 03:17:45 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,8 +54,11 @@ with st.sidebar:
           'thalach': thalach,
           'oldpeak': oldpeak}
   input_df = pd.DataFrame(data, index=[0])
-  input_penguins = pd.concat([input_df, X], axis=0)
+  input_disease = pd.concat([input_df, X], axis=0)
 
-input_df
-#input_penguins
+with st.expander('**Input Features**'):
+  st.write('**Input Disease**')
+  input_df
+  st.write('**Combined Disease Data**')
+  input_disease
   
