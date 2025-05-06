19c43ded44af07512e2d6ba806b782e4d6bad00d
DwaneGarcia <garcia.dwanelloyd@gmail.com>
2025-05-07 00:35:05 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ with st.sidebar:
   st.header('Input Features')
   #Age","Sex","HighChol","CholCheck","BMI","Smoker","HeartDiseaseorAttack","PhysActivity","Fruits","Veggies","HvyAlcoholConsump","GenHlth","MentHlth","PhysHlth","DiffWalk"
   Age = st.selectbox('Age', ('1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13'))
-  Sex = st.selectboc('Sex', ('0', '1'))
+  Sex = st.selectbox('Sex', ('0', '1'))
 
 
   
